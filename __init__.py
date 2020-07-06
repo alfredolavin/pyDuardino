32823f97d344d2f46eaf0dbca23d57adaf40aa22
usr <usr@usr-main>
2020-07-06 17:11:04 -0400

    Just a rename

--- __init__.py
+++ __init__.py
@@ -79,97 +79,112 @@ class OrderedDotDict(OrderedDict):
   __setattr__ = OrderedDict.__setitem__
   __delattr__ = OrderedDict.__delitem__
 
-def Nones(n=10):
-  return [None]*n
+def Nones(self, n= 10):
+  return forEach([None,]*(n or self.len))
 
 def column(self, *names):
   if isinstance(names, str):
     names = [str(names),]
   if len(names) == 1:
     names= names[0]
-    return [getattr(i, names) for i in self]
+    return forEach([getattr(i, names) for i in self])
   else:
-    return [[getattr(i, name) for name in names] for i in self]
+    return forEach([[getattr(i, name) for name in names] for i in self])
+
+def row(self, *names):
+  if isinstance(names, str):
+    names = [str(names),]
+  if len(names) == 1:
+    names= names[0]
+    return forEach([getattr(i, names) for i in self])
+  else:
+    return forEach([[getattr(i, name) for name in names] for i in self])
+
+def positionEvens(self):
+  return forEach(self)[1::2]
+
+def positionOdds(self):
+  return forEach(self)[::2]
 
 def evens(self):
-  return self[1::2]
+  return forEach(filter(lambda x: x%2!=0,self))
 
 def odds(self):
-  return self[::2]
+  return forEach(filter(lambda x: x%2==0,self))
+
+def numbers(self):
+  return forEach((self)[1::2])
 
 def last(self):
-  return self[-1]
+  return (self)[-1]
+
+def middle(self):
+  return self[self.len//2]
 
 def first(self):
   return self[0]
 
 def _reversed(self):
-  return reversed(self)
+  return forEach(reversed(self))
 
 def _len(self):
-  return len(self)
+  return len(list(self))
 
 def exclude(self, *what):
+  tmp= forEach(self)
   for item in what:
     try:
-      self.remove(item)
+      tmp.remove(item)
     except ValueError:
       pass
-  return self
+  return tmp
+
+def _map(self, func):
+  tmp= forEach(self)
+  for i in range(tmp):
+    tmp[i]= eval(func)
+  return tmp
+
+def enumerateme(self, func):
+  return forEach(enumerate(self))
 
 def where(self, what):
   if callable(what):
-    return filter(what, self)
+    return forEach(filter(what, self))
   elif isinstance(what, str):
     filter_code = '[item for item in self if item %s]'%what
-    return eval(filter_code)
+    return forEach(eval(filter_code))
   else:
     raise TypeError()
 
 def eduardinze():
-  proxy_builtin( range )['forEach'] = property(forEach)
-  proxy_builtin( str )['re'] = property(_re)
-  # Extend lists
-  proxy_builtin( list )['forEach'] = property(forEach)
-  proxy_builtin( list )['len'] = property(_len)
-  proxy_builtin( list )['exclude'] = exclude
-  proxy_builtin( list )['column'] = column
-  proxy_builtin( list )['evens'] = property(evens)
-  proxy_builtin( list )['where'] = property(where)
-  proxy_builtin( list )['odds'] = property(odds)
-  proxy_builtin( list )['last'] = property(last)
-  proxy_builtin( list )['first'] = property(first)
-  proxy_builtin( list )['reversed'] = property(_reversed)
-  # Extend ranges too
-  proxy_builtin( range )['forEach'] = property(forEach)
-  proxy_builtin( range )['len'] = property(_len)
-  proxy_builtin( range )['exclude'] = exclude
-  proxy_builtin( range )['column'] = column
-  proxy_builtin( range )['evens'] = evens
-  proxy_builtin( range )['where'] = where
-  proxy_builtin( range )['odds'] = odds
-  proxy_builtin( range )['last'] = last
-  proxy_builtin( range )['first'] = first
-  proxy_builtin( range )['reversed'] = _reversed
-
-  # Extend ranges too
-  proxy_builtin( range )['forEach'] = property(forEach)
-  proxy_builtin( range )['len'] = property(_len)
-  proxy_builtin( range )['exclude'] = exclude
-  proxy_builtin( range )['column'] = column
-  proxy_builtin( range )['evens'] = evens
-  proxy_builtin( range )['where'] = where
-  proxy_builtin( range )['odds'] = odds
-  proxy_builtin( range )['last'] = last
-  proxy_builtin( range )['first'] = first
-  proxy_builtin( range )['reversed'] = _reversed
+  for built_in_class in (list, tuple, range,):
+    proxy_builtin( built_in_class )['positionEvens']= property(positionEvens)
+    proxy_builtin( built_in_class )['positionOdds']= property(positionOdds)
+    proxy_builtin( built_in_class )['enumerateme']= property(enumerateme)
+    proxy_builtin( built_in_class )['reversed']= property(_reversed)
+    proxy_builtin( built_in_class )['exclude']= property(exclude)
+    proxy_builtin( built_in_class )['forEach']= property(forEach)
+    proxy_builtin( built_in_class )['numbers']= property(numbers)
+    proxy_builtin( built_in_class )['column']= property(column)
+    proxy_builtin( built_in_class )['middle']= property(middle)
+    proxy_builtin( built_in_class )['evens']= property(evens)
+    proxy_builtin( built_in_class )['first']= property(first)
+    proxy_builtin( built_in_class )['Nones']= property(Nones)
+    proxy_builtin( built_in_class )['where']= property(where)
+    proxy_builtin( built_in_class )['last']= property(last)
+    proxy_builtin( built_in_class )['odds']= property(odds)
+    proxy_builtin( built_in_class )['len']= property(_len)
+    proxy_builtin( built_in_class )['map']= property(_map)
+    proxy_builtin( built_in_class )['row']= property(row)
 
 eduardinze()
 
 def main():
   # Run some test and examples
-  sample = list(range(0,10,3)) + ['a','b']
-  print((list(range(0,10,3)) + ['a','b']).odds().str().upper())
+  
+  
+  sample = list(range(0,10,3))
 
   test = list(range(10)) + ['a','b']
   print(test.forEach.str().upper())
